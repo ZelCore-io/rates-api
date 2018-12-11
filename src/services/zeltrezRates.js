f766d62cde8d9ab4cfa643c31d1d159de7832f28
TheTrunk <tadeas.kmenta@zel.cash>
2018-12-11 17:40:53 +0100

    rewrite Rates One failed promise does not fail rates service show and handle errors

--- src/services/zeltrezRates.js
+++ src/services/zeltrezRates.js
@@ -1,52 +1,109 @@
 const request = require('request-promise-native');
 
-const zeltrezRates = {
+function apiRequest(url) {
+  return request({ uri: url, json: true })
+    .then((response) => {
+      return response
+    })
+    .catch((error) => {
+      console.log("ERROR: " + url)
+      return error
+    })
+}
+
+var zeltrezRates = {
   getAll() {
     return Promise.all([
-      request({ uri: 'https://min-api.cryptocompare.com/data/pricemulti?fsyms=TOK,CONI,PAX,GUSD,USDC,ETC,XMR,DASH,BTC,ETH,ZEC,USDT,LTC,BTCZ,RVN,BCH,BNB,BTX,SONM,OMG,ZIL,ZRX,GNT,SPHTX,BAT,MKR,KNC,ENG,PAY,SUB,CVC,STX,BTG,KCS,SRN&tsyms=BTC', json: true }),
-      request({ uri: 'https://min-api.cryptocompare.com/data/pricemulti?fsyms=HUSH,ZCL,XSG,BTCP,ZEN,KMD,XZC,ZER,ABT,ADX,AE,AION,AST,BBO,APPC,BLZ,BNT,ETHOS,COFI,DAI,DGX,ELEC,ELF,ENJ,STORJ,IOST,DENT,LEND,LINK,MANA,LRC,QASH,ICN,MCO,MTL,POE,POLY,POWR,RCN,RDN,REQ,SNT,SALT,STORM,EDO,TUSD,DCN,WAX,WINGS,DTA,FUN,KIN,BSV,AOA,THETA,ADT,MFT,ATL,ANT,ARN,BRD,REP,QKC,LOOM&tsyms=BTC', json: true }),
-      request({ uri: 'https://bitpay.com/api/rates', json: true }),
-      request({ uri: 'https://www.worldcoinindex.com/apiservice/ticker?key=pZURzUjb0QFbY9knkicp3rrOqwYdwn&label=safebtc&fiat=btc', json: true }),
-      request({ uri: 'https://api.coinmarketcap.com/v1/ticker/zelcash/', json: true }),
-      request({ uri: 'https://www.cryptopia.co.nz/api/GetMarket/ANON_BTC', json: true }),
-      request({ uri: 'https://api.coinmarketcap.com/v1/ticker/suqa/', json: true }),
-      request({ uri: 'https://tradesatoshi.com/api/public/getticker?market=GENX_BTC', json: true }),
-      request({ uri: 'https://api.crex24.com/v2/public/tickers?instrument=BZE-BTC', json: true }),
-      request({ uri: 'https://coinlib.io/api/v1/coin?key=38bc7ea5cf2b6231&pref=BTC&symbol=POR', json: true })
+      apiRequest('https://min-api.cryptocompare.com/data/pricemulti?fsyms=TOK,CONI,PAX,GUSD,USDC,ETC,XMR,DASH,BTC,ETH,ZEC,USDT,LTC,BTCZ,RVN,BCH,BNB,BTX,SONM,OMG,ZIL,ZRX,GNT,SPHTX,BAT,MKR,KNC,ENG,PAY,SUB,CVC,STX,BTG,KCS,SRN&tsyms=BTC'),
+      apiRequest('https://min-api.cryptocompare.com/data/pricemulti?fsyms=HUSH,ZCL,XSG,BTCP,ZEN,KMD,XZC,ZER,ABT,ADX,AE,AION,AST,BBO,APPC,BLZ,BNT,ETHOS,COFI,DAI,DGX,ELEC,ELF,ENJ,STORJ,IOST,DENT,LEND,LINK,MANA,LRC,QASH,ICN,MCO,MTL,POE,POLY,POWR,RCN,RDN,REQ,SNT,SALT,STORM,EDO,TUSD,DCN,WAX,WINGS,DTA,FUN,KIN,BSV,AOA,THETA,ADT,MFT,ATL,ANT,ARN,BRD,REP,QKC,LOOM&tsyms=BTC'),
+      apiRequest('https://bitpay.com/api/rates'),
+      apiRequest('https://www.worldcoinindex.com/apiservice/ticker?key=pZURzUjb0QFbY9knkicp3rrOqwYdwn&label=safebtc&fiat=btc'),
+      apiRequest('https://api.coinmarketcap.com/v1/ticker/zelcash/'),
+      apiRequest('https://www.cryptopia.co.nz/api/GetMarket/ANON_BTC'),
+      apiRequest('https://api.coinmarketcap.com/v1/ticker/suqa/'),
+      apiRequest('https://tradesatoshi.com/api/public/getticker?market=GENX_BTC'),
+      apiRequest('https://api.crex24.com/v2/public/tickers?instrument=BZE-BTC'),
+      apiRequest('https://coinlib.io/api/v1/coin?key=38bc7ea5cf2b6231&pref=BTC&symbol=POR')
     ]).then((results) => {
-      const ccDataA = results[0]; // results from cryptocompare
-      const ccDataB = results[1]; // results from cryptocompare
-      const bitpayData = results[2]; // results from bitpay
-      const safeprice = results[3].Markets[0].Price
-      const zelprice = Number(results[4][0].price_btc)
-      const anonprice = results[5].Data.LastPrice
-      const suqaprice = Number(results[6][0].price_btc)
-      const genxprice = results[7].result.last
-      const bzeprice = results[8][0].last
-      const porprice = Number(results[9].price)
+      var rates = [];
+      var efg = {};
+      var errors = { errors: {} }
+      var ccDataA = results[0]; // results from cryptocompare
+      var ccDataB = results[1]; // results from cryptocompare
+      try {
+        var dummyTest = results[2][1].code; // results from bitpay
+        var bitpayData = results[2]
+      } catch (e) {
+        var bitpayData = -1
+        errors.errors.bitPayData = results[2]
+      }
 
-      const rates = [];
-      const efg = {}
+      try {
+        var safeprice = results[3].Markets[0].Price
+        efg.SAFE = safeprice
+      } catch (e) {
+        errors.errors.SAFE = results[3]
+      }
+      try {
+        var zelprice = Number(results[4][0].price_btc)
+        efg.ZEL = zelprice
+      } catch (e) {
+        errors.errors.ZEL = results[4]
+      }
+      try {
+        var anonprice = results[5].Data.LastPrice
+        efg.ANON = anonprice
+      } catch (e) {
+        errors.errors.ANON = results[5]
+      }
+      try {
+        var suqaprice = Number(results[6][0].price_btc)
+        efg.SUQA = suqaprice
+      } catch (e) {
+        errors.errors.suqa = results[6]
+      }
+      try {
+        var genxprice = results[7].result.last
+        efg.GENX = genxprice
+      } catch (e) {
+        errors.errors.GENX = results[7]
+      }
+      try {
+        var bzeprice = results[8][0].last
+        efg.BZE = bzeprice
+      } catch (e) {
+        errors.errors.BZE = results[8]
+      }
+      try {
+        var eztest = results[9].markets[0].symbol
+        var porprice = Number(results[9].price)
+        efg.POR = porprice
+      } catch (e) {
+        errors.errors.POR = results[9]
+      }
 
-      const coinsA = Object.keys(ccDataA)
+      var coinsA = Object.keys(ccDataA)
       coinsA.forEach((coin) => {
-        efg[coin] = ccDataA[coin].BTC
+        try {
+          efg[coin] = ccDataA[coin].BTC
+        } catch (e) {
+          errors.errors.coinsA = results[0]
+        }
       })
 
-      const coinsB = Object.keys(ccDataB)
+      var coinsB = Object.keys(ccDataB)
       coinsB.forEach((coin) => {
-        efg[coin] = ccDataB[coin].BTC
+        try {
+          efg[coin] = ccDataB[coin].BTC
+        } catch (e) {
+          errors.errors.coinsB = results[1]
+        }
       })
 
-      efg.SAFE = safeprice
-      efg.ZEL = zelprice
-      efg.ANON = anonprice
-      efg.SUQA = suqaprice
-      efg.GENX = genxprice
-      efg.BZE = bzeprice
-      efg.POR = porprice
       rates.push(bitpayData);
-      rates.push(efg)
+      rates.push(efg);
+
+      rates.push(errors);
 
       return rates;
     });
