2ab4430971c0d4ab8c1d48e91f7265c7b1235a6a
stultusmundi <bmg.stultusmundi@gmail.com>
2021-09-16 13:47:44 +0300

    Rework on rates api.

--- src/services/zelcoreMarkets.js
+++ src/services/zelcoreMarkets.js
@@ -16,144 +16,58 @@ function apiRequest(url) {
 var zelcoreMarkets = {
   getAll() {
     return Promise.all([
-      //  marketinfo
-      apiRequest(`https://min-api.cryptocompare.com/data/pricemultifull?fsyms=TOK,CONI,PAX,GUSD,USDC,ETC,XMR,DASH,BTC,ETH,ZEC,USDT,LTC,BTCZ,RVN,BCH,BNB,BTX,SONM,OMG,ZIL,ZRX,GNT,SPHTX,BAT,MKR,ENG,PAY,SUB,CVC,STX,BTG,KCS,SRN,EVX,GTO,GVT,INS,IOTX,KEY,LUN,MDA,MITH,MTH,OAX,OST,PPT,QSP,REN,RLC,SNGLS,TNB,TNT,VIB,VIBE,WABI,WPR,DOCK,FUEL,CDT,CELR,CND,DATA,DGD,DLT,AGI&tsyms=BTC&api_key=${apiKey}`),  //  0
-      apiRequest(`https://min-api.cryptocompare.com/data/pricemultifull?fsyms=HUSH,TENT,BTCP,ZEN,KMD,XZC,ABT,ADX,AE,AION,AST,BBO,APPC,BLZ,BNT,ETHOS,COFI,DAI,DGX,ELEC,ELF,ENJ,STORJ,IOST,DENT,LEND,LINK,MANA,LRC,QASH,ICN,MCO,MTL,POE,POLY,POWR,RCN,REQ,SNT,SALT,STORM,EDO,TUSD,DCN,WAX,WINGS,DTA,FUN,KIN,BSV,AOA,THETA,ADT,MFT,ATL,ANT,ARNX,BRD,REP,QKC,LOOM,ANON,EURS,AMB,BCPT&tsyms=BTC&api_key=${apiKey}`), //  1
-      apiRequest(`https://min-api.cryptocompare.com/data/pricemultifull?fsyms=SIN,MER,ALEPH,FLUX,ER,QTUM,XEM,ONGAS,ONT,MIOTA,GAS,TRX,DGB,XLM,DOGE,EOS,ADA,XRP,DOCK,NEO,BTT,GRS,XCASH,LEO,USDS,ENQ,FTM,0XBTC,AERGO,UBT,ILC,HEX,COMP,VIDT,DRGN,WBTC,OM,UNI,JST,BDX,FIRO,CAKE,MATIC,ZCL,VBK,STETH,AMP,TEL,ONE,AVAX,ATOM,AXS,XTZ,BTCB,SHIB,UST,YFI,SNX,NEAR,C98,ANKR,SXP,WRX&tsyms=BTC&api_key=${apiKey}`), // 2
-      // marketinfo CoinGecko
-      apiRequest('https://api.coingecko.com/api/v3/coins/markets?vs_currency=btc&ids=solfarm,cope,bonfida,maps,media-network,oxygen,raydium,step-finance,rope-token,presearch,kyber-network,kyber-network-crystal,solana,serum,gatechain-token,snowgem,zclassic,1inch,hotbit-token,binance-usd,huobi-pool-token,huobi-token,zb-token,mx-token,bitforex,okb,veriblock,dmme,suqa,holotoken,half-life,axe,safe-coin-2,genesis-network,bzedge,commercium,bitcoin-zero,zelcash,kadena,whale,golfcoin,alpha-finance&order=market_cap_desc&per_page=100&page=1&sparkline=false'), // 3
-      apiRequest('https://api.coingecko.com/api/v3/coins/markets?vs_currency=btc&ids=polkadot,kusama,nxm,just-stablecoin,sun-token,chiliz,gnosis,cybervein,husd,ocean-protocol,quant-network,hedgetrade,terrausd,reserve-rights-token,ampleforth,swissborg,renbtc,uma,crypto-com-chain,celsius-degree-token,sushi,the-graph,ftx-token,yearn-finance,havven,aave,revain,xdai-stake,dai,nexo,true-usd,thorchain,bitcoin-bep2,maidsafecoin,bakerytoken,safemoon,huplife,raptoreum,axie-infinity,vertcoin,lido-staked-ether,amp,telcoin,harmony&order=market_cap_desc&per_page=100&page=1&sparkline=false'), // 4
-      apiRequest('https://api.coingecko.com/api/v3/coins/markets?vs_currency=btc&ids=waves,perpetual-protocol,audius,curve-dao-token,the-sandbox,fetch-ai,golem,neutrino,skale,numeraire,livepeer,my-neighbor-alice,fei-protocol,origin-protocol,injective-protocol,singularitynet,pax-gold,band-protocol,storm,reef-finance,cartesi,nkn&order=market_cap_desc&per_page=100&page=1&sparkline=false'), // 5
-      // apiRequest('https://min-api.cryptocompare.com/data/pricemultifull?fsyms=BTC&tsyms=BTC'), // give me price of BTC in BTC 4
-      //apiRequest('https://api.coingecko.com/api/v3/coins/markets?vs_currency=btc&ids=genesis-network&order=market_cap_desc&per_page=100&page=1&sparkline=false'),
-      //apiRequest('https://api.coingecko.com/api/v3/coins/markets?vs_currency=btc&ids=bzedge&order=market_cap_desc&per_page=100&page=1&sparkline=false'),
-      //apiRequest('https://api.coingecko.com/api/v3/coins/markets?vs_currency=btc&ids=commercium&order=market_cap_desc&per_page=100&page=1&sparkline=false'),
-      //apiRequest('https://api.coingecko.com/api/v3/coins/markets?vs_currency=btc&ids=bitcoin-zero&order=market_cap_desc&per_page=100&page=1&sparkline=false'),
-      // apiRequest('https://api.coinpaprika.com/v1/ticker/golf-golfcoin'),  // 5
-
-
-    ]).then((results) => {
+     //  marketinfo cryptocompare
+    ].concat(cryptoCompareIDs.map(async element => {
+      return apiRequest(`https://min-api.cryptocompare.com/data/pricemultifull?fsyms=${element}&tsyms=BTC&api_key=${apiKey}`)//
+      //  marketinfo coingecko
+    })).concat(coingeckoIDs.map(async element => { 
+      return apiRequest(`https://api.coingecko.com/api/v3/coins/markets?vs_currency=btc&ids=${element}&order=market_cap_desc&per_page=100&page=1&sparkline=false`)
+    }))
+    ).then((results) => {
       var markets = [];
       var cmk = {};
       var errors = { errors: {} }
 
-      //marketcap Data
-      var ccDataFullA = results[0]; // full results from cryptocompare
-      var ccDataFullB = results[1]; // full results from cryptocompare
-      var ccDataFullC = results[2]; // full results from cryptocompare
-      var ccDataFullD = results[3]; // full results from coingecko
-      var ccDataFullE = results[4]; // full results from coingecko
-      var ccDataFullF = results[5]; // full results from coingecko
-      // var btcPrice = results[4];
-      // var ccDataFullE = results[5]; // full results from coinpaprika
-      // var btcPrice = results[4];
-      // var btcInUsd = 5300;
-      // try {
-      //   btcInUsd = btcPrice.RAW.BTC.BTC.PRICE;
-      // } catch (e) {
-      //   errors.errors.btcerror = results[4];
-      // }
-
-      var coinsFullA = Object.keys(ccDataFullA.RAW) // full results from cryptocompare
-      coinsFullA.forEach((coin) => {
-        try {
-          var coindetail = {}
-          coindetail['supply'] = ccDataFullA.RAW[coin].BTC.SUPPLY
-          coindetail['volume'] = ccDataFullA.RAW[coin].BTC.TOTALVOLUME24HTO
-          coindetail['change'] = ccDataFullA.RAW[coin].BTC.CHANGEPCT24HOUR
-          coindetail['market'] = ccDataFullA.RAW[coin].BTC.MKTCAP
-          cmk[coin] = coindetail
-        } catch (e) {
-          errors.errors.coinsFullA = results[0]
-        }
+      
+      // full results from cryptocompare
+      var dataCC = results.slice(0,cryptoCompareIDs.length);
+      dataCC.forEach((subresult) => {
+        var coinsFull = Object.keys(subresult.RAW) // full results from cryptocompare
+        coinsFull.forEach((coin) => {
+          try {
+            var coindetail = {}
+            coindetail['supply'] = subresult.RAW[coin].BTC.SUPPLY
+            coindetail['volume'] = subresult.RAW[coin].BTC.TOTALVOLUME24HTO
+            coindetail['change'] = subresult.RAW[coin].BTC.CHANGEPCT24HOUR
+            coindetail['market'] = subresult.RAW[coin].BTC.MKTCAP
+            cmk[coin] = coindetail
+          } catch (e) {
+            errors.errors.coinsFull = subresult
+          }
+        })
       })
 
-      var coinsFullB = Object.keys(ccDataFullB.RAW) // full results from cryptocompare
-      coinsFullB.forEach((coin) => {
-        try {
-          var coindetail = {}
-          coindetail['supply'] = ccDataFullB.RAW[coin].BTC.SUPPLY
-          coindetail['volume'] = ccDataFullB.RAW[coin].BTC.TOTALVOLUME24HTO
-          coindetail['change'] = ccDataFullB.RAW[coin].BTC.CHANGEPCT24HOUR
-          coindetail['market'] = ccDataFullB.RAW[coin].BTC.MKTCAP
-          cmk[coin] = coindetail
-        } catch (e) {
-          errors.errors.coinsFullB = results[1]
-        }
+      // full results from coingecko
+      dataCG = results.slice(cryptoCompareIDs.length,cryptoCompareIDs.length + coingeckoIDs.length);
+      dataCG.forEach((subresult) => {
+        var coinsFull = Object.keys(subresult) 
+        coinsFull.forEach((coin) => {
+          try {
+            var coindetail = {}
+            coindetail['rank'] = subresult[coin].market_cap_rank
+            coindetail['total_supply'] = subresult[coin].total_supply
+            coindetail['supply'] = subresult[coin].circulating_supply
+            coindetail['volume'] = subresult[coin].total_volume
+            coindetail['change'] = subresult[coin].price_change_percentage_24h
+            coindetail['market'] = subresult[coin].market_cap
+            cmk[subresult[coin].symbol.toUpperCase()] = coindetail
+          } catch (e) {
+            errors.errors.coinsFull = subresult
+          }
+        })
       })
 
-      var coinsFullC = Object.keys(ccDataFullC.RAW) // full results from cryptocompare
-      coinsFullC.forEach((coin) => {
-        try {
-          var coindetail = {}
-          coindetail['supply'] = ccDataFullC.RAW[coin].BTC.SUPPLY
-          coindetail['volume'] = ccDataFullC.RAW[coin].BTC.TOTALVOLUME24HTO
-          coindetail['change'] = ccDataFullC.RAW[coin].BTC.CHANGEPCT24HOUR
-          coindetail['market'] = ccDataFullC.RAW[coin].BTC.MKTCAP
-          cmk[coin] = coindetail
-        } catch (e) {
-          errors.errors.coinsFullC = results[2]
-        }
-      })
-
-      var coinsFullD = Object.keys(ccDataFullD) // full results from coingecko
-      coinsFullD.forEach((coin) => {
-        try {
-          var coindetail = {}
-          coindetail['rank'] = ccDataFullD[coin].market_cap_rank
-          coindetail['total_supply'] = ccDataFullD[coin].total_supply
-          coindetail['supply'] = ccDataFullD[coin].circulating_supply
-          coindetail['volume'] = ccDataFullD[coin].total_volume
-          coindetail['change'] = ccDataFullD[coin].price_change_percentage_24h
-          coindetail['market'] = ccDataFullD[coin].market_cap
-          cmk[ccDataFullD[coin].symbol.toUpperCase()] = coindetail
-        } catch (e) {
-          errors.errors.coinsFullD = results[3]
-        }
-      })
-
-      var coinsFullE = Object.keys(ccDataFullE) // full results from coingecko
-      coinsFullE.forEach((coin) => {
-        try {
-          var coindetail = {}
-          coindetail['rank'] = ccDataFullE[coin].market_cap_rank
-          coindetail['total_supply'] = ccDataFullE[coin].total_supply
-          coindetail['supply'] = ccDataFullE[coin].circulating_supply
-          coindetail['volume'] = ccDataFullE[coin].total_volume
-          coindetail['change'] = ccDataFullE[coin].price_change_percentage_24h
-          coindetail['market'] = ccDataFullE[coin].market_cap
-          cmk[ccDataFullE[coin].symbol.toUpperCase()] = coindetail
-        } catch (e) {
-          errors.errors.coinsFullE = results[4]
-        }
-      })
-
-      var coinsFullF = Object.keys(ccDataFullF) // full results from coingecko
-      coinsFullF.forEach((coin) => {
-        try {
-          var coindetail = {}
-          coindetail['rank'] = ccDataFullF[coin].market_cap_rank
-          coindetail['total_supply'] = ccDataFullF[coin].total_supply
-          coindetail['supply'] = ccDataFullF[coin].circulating_supply
-          coindetail['volume'] = ccDataFullF[coin].total_volume
-          coindetail['change'] = ccDataFullF[coin].price_change_percentage_24h
-          coindetail['market'] = ccDataFullF[coin].market_cap
-          cmk[ccDataFullF[coin].symbol.toUpperCase()] = coindetail
-        } catch (e) {
-          errors.errors.coinsFullF = results[5]
-        }
-      })
-
-      // try {
-      //   var coindetail = {}
-      //   coindetail['supply'] = Number(ccDataFullE.circulating_supply)
-      //   coindetail['volume'] = Number(ccDataFullE.volume_24h_usd) / btcPrice.RAW.BTC.BTC.PRICE
-      //   coindetail['change'] = Number(ccDataFullE.percent_change_24h)
-      //   coindetail['market'] = Number(ccDataFullE.market_cap_usd) / btcPrice.RAW.BTC.BTC.PRICE
-      //   cmk[ccDataFullE.symbol.toUpperCase()] = coindetail
-      // } catch (e) {
-      //   errors.errors.coinsFullD = results[5]
-      // }
+      // Some wrapped assets and flux
       cmk.ONG = cmk.ONGAS;
       cmk.SAI = cmk.DAI;
       cmk.WBNB = cmk.BNB;
