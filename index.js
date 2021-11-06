d1ca96581a0832a855d85472053d26bdfe4b550d
Stultus Mundi <bmg.stultusmundi@gmail.com>
2021-11-06 14:55:15 +0200

    Fixes to eslint conforming

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = require('./src/lib/server');
 const log = require('./src/lib/log');
 
 const server = http.createServer(app);
-const port = config.server.port
+const { port } = config.server;
 
 server.listen(port, () => {
   log.info(`rates-api launched, listening on port ${port}!`);
