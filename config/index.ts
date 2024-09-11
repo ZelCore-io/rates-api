23760b9e373afe24abd28071964654d5f4d34f67
Stultus Mundi <bmg.stultusmundi@gmail.com>
2024-09-11 16:38:42 +0300

    Adding apikey and api URL

--- config/index.ts
+++ config/index.ts
@@ -3,6 +3,8 @@ export const config = {
     port: 3333,
   },
   apiKey: '9789a5cea8c4dafa840fb549cd092c2739131128fc58cad70007085aa6e29b34',
+  coinGeckoKey: '',
+  coinGeckoUrl: 'https://pro-api.coingecko.com/api/v3/',
 };
 
 export default config;
