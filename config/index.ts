36b402665c33a07284fa221bf95b03f5397fed63
Stultus Mundi <bmg.stultusmundi@gmail.com>
2024-09-11 13:15:43 +0300

    refactor config

--- config/index.ts
+++ config/index.ts
@@ -1,6 +1,8 @@
-module.exports = {
+export const config = {
   server: {
     port: 3333,
   },
   apiKey: '9789a5cea8c4dafa840fb549cd092c2739131128fc58cad70007085aa6e29b34',
 };
+
+export default config;
