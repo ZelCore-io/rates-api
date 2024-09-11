4639258c0872b0fb0604fcba03553239392bac82
Stultus Mundi <bmg.stultusmundi@gmail.com>
2024-09-11 18:05:53 +0300

    Adding makeRequestStrings to utils

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -5,3 +5,25 @@ export function arraySplit(arr: string[], size: number): string[][] {
   }
   return result;
 }
+
+/**
+ * Function that combines with commas the elements of a string array till some max length and returns the new array of strings
+ * @param {string[]} elements
+ * @param {number} maxLength
+ * @returns string[]
+ */
+export function makeRequestStrings(elements: string[], maxLength: number): string[] {
+  const result: string[] = [];
+  let temp = '';
+  elements.forEach((element) => {
+    if ((`${temp + element},`).length >= maxLength) {
+      result.push(temp.replace(/,\s*$/, '')); // remove last ,
+      temp = `${element},`;
+    } else if (element === elements[elements.length - 1]) {
+      result.push(temp + element);
+    } else {
+      temp = `${temp + element},`;
+    }
+  });
+  return result;
+}
