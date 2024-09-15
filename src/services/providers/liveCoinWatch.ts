3cdac537dc51d0f0b73a32086e0f72b1eb20572c
Stultus Mundi <bmg.stultusmundi@gmail.com>
2024-09-15 07:43:43 +0300

    Adding liveCoinWatch LRU cache

--- src/services/providers/liveCoinWatch.ts
+++ src/services/providers/liveCoinWatch.ts
@@ -1,6 +1,7 @@
 import AxiosWrapper from "../../lib/axios";
 import config from "../../../config";
 import { makeRequestStrings } from "../../lib/utils";
+import { LRUCache as LRU } from 'lru-cache';
 
 const MAX_LENGTH_PER_REQUEST = 300;
 
@@ -12,13 +13,21 @@ export class LiveCoinWatch {
     'Content-Type': 'application/json',
     'x-api-key': `${this.apiKey}`,
   };
+  private cache: LRU<string, any>;
+
   constructor() {
     if (LiveCoinWatch.instance) {
       throw new Error("Use Singleton.getInstance()");
     }
     LiveCoinWatch.instance = this;
     LiveCoinWatch.axiosWrapper = new AxiosWrapper(config.liveCoinWatchUrl);
+
+    this.cache = new LRU({
+      max: 100, // Maximum number of items in the cache
+      ttl: 1000 * 60 * 1, // Cache for 1 minutes
+    });
   }
+
   public static getInstance(): LiveCoinWatch {
     return LiveCoinWatch.instance || new LiveCoinWatch();
   }
@@ -30,7 +39,14 @@ export class LiveCoinWatch {
     });
   }
 
-  private async _getExchangeRates(ids: string, vsCurrency = 'BTC', ): Promise<any> {
+  private async _getExchangeRates(ids: string, vsCurrency = 'BTC'): Promise<any> {
+    const cacheKey = `exchangeRates_${ids}_${vsCurrency}`;
+    const cachedData = this.cache.get(cacheKey);
+
+    if (cachedData) {
+      return cachedData;
+    }
+
     const data = {
       currency: vsCurrency,
       codes: ids,
@@ -40,17 +56,24 @@ export class LiveCoinWatch {
       limit: 0,
       meta: true,
     };
+
     const response = await this.post('coins/map', data);
-    return response.data;
+    const responseData = response.data;
+
+    this.cache.set(cacheKey, responseData);
+
+    return responseData;
   }
 
   public async getExchangeRates(ids: string[], vsCurrency = 'BTC'): Promise<any> {
     const newIds = makeRequestStrings(ids, MAX_LENGTH_PER_REQUEST);
     let allRates = {};
+    
     for (const id of newIds) {
       const response = await this._getExchangeRates(id, vsCurrency);
       allRates = { ...allRates, ...response };
     }
+
     return allRates;
   }
 }
