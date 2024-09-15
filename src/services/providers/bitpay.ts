e3d510762561b244e6e8e37ac23617501d401cb9
Stultus Mundi <bmg.stultusmundi@gmail.com>
2024-09-15 07:41:41 +0300

    Adding bitpay LRU caching

--- src/services/providers/bitpay.ts
+++ src/services/providers/bitpay.ts
@@ -1,5 +1,6 @@
 import AxiosWrapper from "../../lib/axios";
 import config from "../../../config";
+import { LRUCache as LRU } from 'lru-cache';
 
 export class BitPay {
   private static instance: BitPay;
@@ -7,13 +8,21 @@ export class BitPay {
   private readonly headers = {
     'Content-Type': 'application/json',
   };
+  private cache: LRU<string, any>;
+
   constructor() {
     if (BitPay.instance) {
       throw new Error("Use Singleton.getInstance()");
     }
     BitPay.instance = this;
     BitPay.axiosWrapper = new AxiosWrapper(config.bitPayUrl);
+
+    this.cache = new LRU({
+      max: 100, // Maximum number of items in the cache
+      ttl: 1000 * 60 * 1, // Time to live in milliseconds (1 minutes)
+    });
   }
+
   public static getInstance(): BitPay {
     return BitPay.instance || new BitPay();
   }
@@ -26,9 +35,20 @@ export class BitPay {
   }
 
   public async getFiatRates(): Promise<any | null> {
+    const cacheKey = 'fiatRates';
+    const cachedRates = this.cache.get(cacheKey);
+    
+    if (cachedRates) {
+      return cachedRates;
+    }
+
     try {
       const response = await this.get('rates/BTC');
-      return response.data.data;
+      const data = response.data.data;
+
+      this.cache.set(cacheKey, data);
+      
+      return data;
     } catch (error) {
       return null;
     }
