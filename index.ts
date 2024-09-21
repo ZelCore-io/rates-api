f81948a87727e651853c186292a1b637a0eb59e0
Stultus Mundi <bmg.stultusmundi@gmail.com>
2024-09-21 18:41:43 +0300

    Adding typedoc comments for index

--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-import 'dotenv/config'
+import 'dotenv/config';
 import * as http from 'http';
 import config from './config';
 import app from './src/lib/server';
@@ -8,24 +8,30 @@ import apiServices from './src/services/apiServices';
 const server = http.createServer(app);
 const { port } = config.server;
 
+/**
+ * Starts the HTTP server when data is available.
+ *
+ * This function checks if the required data is loaded before starting the server.
+ * If data is not yet available, it retries after a delay.
+ */
 function startService(): void {
   const data = apiServices.getData();
-  console.log("startService -> data", !!data.rates[0][0], !!data.marketsUSD[0])
+  console.log("startService -> data", !!data.rates[0][0], !!data.marketsUSD[0]);
   if (data.rates[0][0] && data.marketsUSD[0]) {
     setTimeout(() => {
       server.listen(port, () => {
         log.info(`rates-api launched, listening on port ${port}!`);
       });
-    }, 1 * 1000);
+    }, 1000); // Wait for 1 second before starting the server
   } else {
     setTimeout(() => {
       startService();
-    }, 1 * 1000);
+    }, 1000); // Retry after 1 second
   }
 }
 
 log.info('Starting data refresher');
-// First init by fetching data from repositories
+// First, initialize by fetching data from repositories
 apiServices.dataRefresher().then(() => {
   // Then start services
   log.info('Starting services');
--- src/services/providers/index.ts
+++ src/services/providers/index.ts
@@ -1,4 +1,4 @@
 export { CoinGecko } from './coinGecko';
 export { CryptoCompare } from './cryptoCompare';
 export { BitPay } from './bitpay';
-export { LiveCoinWatch } from './livecoinwatch';
+export { LiveCoinWatch } from './liveCoinWatch';
