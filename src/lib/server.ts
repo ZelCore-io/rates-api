ae6c14896451213044eb79a7f5ca81e436de2608
Stultus Mundi <bmg.stultusmundi@gmail.com>
2024-09-21 19:51:40 +0300

    Adding swagger docs enabling

--- src/lib/server.ts
+++ src/lib/server.ts
@@ -1,8 +1,10 @@
 import express, { Express } from 'express';
 import morgan from 'morgan';
 import cors from 'cors';
+import swaggerUi from 'swagger-ui-express';
+import YAML from 'yamljs';
 
-// If you have route handlers written in TypeScript, import them like this:
+// Import route handlers written in TypeScript
 import routes from '../routes';
 
 /**
@@ -35,7 +37,9 @@ if (nodeEnv !== 'test') {
    * HTTP request logger middleware using 'morgan'.
    *
    * @remarks
-   * The 'combined' format outputs the Apache combined log format.
+   * The 'combined' format outputs the Apache combined log format, logging all HTTP requests.
+   *
+   * @see https://github.com/expressjs/morgan
    */
   app.use(morgan('combined'));
 }
@@ -44,18 +48,39 @@ if (nodeEnv !== 'test') {
  * Cross-Origin Resource Sharing (CORS) middleware.
  *
  * @remarks
- * Enables CORS for all routes by default.
+ * Enables CORS for all routes by default, allowing the application to accept requests from different origins.
  *
  * @see https://github.com/expressjs/cors
  */
 app.use(cors());
 
 /**
- * Registers application routes.
+ * Loads the Swagger documentation from the YAML file and configures the Swagger UI middleware.
  *
  * @remarks
- * The 'routes' function is responsible for setting up all the routes for the application.
+ * The Swagger UI is accessible via the `/api-docs` endpoint and dynamically adjusts the server URL based on the environment.
  *
+ * @see https://swagger.io/tools/swagger-ui/
+ */
+const swaggerDocument = YAML.load('./swagger.yaml');
+const environment = nodeEnv || 'development';
+const baseUrl = process.env.BASE_URL || 'http://localhost:3333';
+
+swaggerDocument.servers = [
+  {
+    url: baseUrl,
+    description: environment === 'production' ? 'Production server' : 'Development server',
+  },
+];
+
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+/**
+ * Registers all application routes.
+ *
+ * @remarks
+ * The `routes` function is responsible for setting up all the necessary routes in the Express application.
+ * 
  * @param app - The Express application instance.
  *
  * @example
